77655c31bed77de41d31fb9971573d1b1dfef2f1
hamedamz <amzhamed@gmail.com>
2021-08-06 11:50:29 +0430

    feat: add rectangle class

---
+++ src/shapes/Rectangle.java
@@ -0,0 +1,11 @@
+package shapes;
+
+public class Rectangle {
+    public int width;
+    public int height;
+
+    public Rectangle(int width, int height) {
+        this.width = width;
+        this.height = height;
+    }
+}
