862888c7e5a950816442a741b29e79e23221218a
hamedamz <amzhamed@gmail.com>
2021-08-06 11:49:52 +0430

    test: rectangle width

---
+++ src/tests/RectangleTest.java
@@ -0,0 +1,19 @@
+package tests;
+
+import org.junit.jupiter.api.Test;
+
+import shapes.Rectangle;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class SELabTest {
+    @Test
+    void testWidth() {
+        int width = 6;
+        int height = 4;
+
+        Rectangle rectangle = new Rectangle(width, height);
+
+        assertEquals(rectangle.width, width);
+    }
+}
